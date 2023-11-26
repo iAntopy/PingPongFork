4deae9a112892d58749ebb3ffc9d374dbfa05080
Ian Antony Mongeon Charest <ian.mongeon42@gmail.com>
2023-11-26 05:36:14 -0500

    master had wrong aliases setup.

--- master.py
+++ master.py
@@ -3,9 +3,9 @@ try:                        import Addons as ad
 except ModuleNotFoundError: import game.PingPongRebound.Addons as ad
 try:                        import GameControler as gc
 except ModuleNotFoundError: import game.PingPongRebound.GameControler as gc
-try:                        import GameObject as gc
+try:                        import GameObject as go
 except ModuleNotFoundError: import game.PingPongRebound.GameObject as go
-try:                        import GameInterface as gc
+try:                        import GameInterface as gi
 except ModuleNotFoundError: import game.PingPongRebound.GameInterface as gi
 # try:                        import cfg
 # except ModuleNotFoundError: import game.PingPongRebound.cfg
